7ff85ec7db65c52ceca3cf064cd0aa767f335b50
jiho lee <jh001007@gmail.com>
2019-02-03 18:03:31 +0900

    단위 추가 & 360도에서 180도로 변경 후 라디안으로 변경

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ struct jumper {
 };
 
 int main() {
+	constexpr double PI = 3.141592;
 	srand(time(NULL));
 	jumper jumpers[100000];
 	int location = rand() % 300 + 200;
 	cout << "location is: " << location << endl;
 	for(int i=0;i<100000;i++) {
 		jumpers[i].mass = rand() % 50 + 50;
-		jumpers[i].angle = (rand() % 36000) / 100.0;
+		jumpers[i].angle = (rand() % 18000) / 100.0 * PI / 180;
 		jumpers[i].speed = (rand() % 10000) / 100.0;
 	}
 	for(int i=0;i<100;i++) {
 		cout << i + 1 << "'th jumper: " << endl;
-		cout << "mass : " << jumpers[i].mass << endl;
-		cout << "angle : " << jumpers[i].angle;
+		cout << "mass : " << jumpers[i].mass << " kg" << endl;
+		cout << "angle : " << jumpers[i].angle << " radian";
 		cout << endl;
 		cout << "speed : " << jumpers[i].speed;
 		cout << endl;
