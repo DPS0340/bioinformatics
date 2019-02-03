bfd201c51514097a00ee67b29154ea3e81654685
jiho lee <jh001007@gmail.com>
2019-02-03 18:14:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -11,21 +12,20 @@ struct jumper {
 
 int main() {
 	constexpr double PI = 3.141592;
+	constexpr double G = 9.81;
 	srand(time(NULL));
 	jumper jumpers[100000];
-	int location = rand() % 300 + 200;
+	int location = rand() % 10000 + 100;
 	cout << "location is: " << location << endl;
 	for(int i=0;i<100000;i++) {
-		jumpers[i].mass = rand() % 50 + 50;
-		jumpers[i].angle = (rand() % 18000) / 100.0 * PI / 180;
-		jumpers[i].speed = (rand() % 10000) / 100.0;
+		jumpers[i].mass = rand() % 70 + 30;
+		jumpers[i].angle = (rand() % 9000) / 100.0 * PI / 180;
+		jumpers[i].speed = (rand() % 100000 + 100) / jumpers[i].mass;
+		jumpers[i].location = jumpers[i].speed * jumpers[i].speed * sin(2*jumpers[i].angle) / G; 
 	}
 	for(int i=0;i<100;i++) {
 		cout << i + 1 << "'th jumper: " << endl;
-		cout << "mass : " << jumpers[i].mass << " kg" << endl;
-		cout << "angle : " << jumpers[i].angle << " radian";
-		cout << endl;
-		cout << "speed : " << jumpers[i].speed;
+		cout << "X location : " << jumpers[i].location;
 		cout << endl;
 		
 	}
